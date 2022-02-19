689fcabd723f995bde81aea8443cf322d2bd21f4
Michał Borowy <mikeborowy@gmail.com>
2022-02-19 14:55:06 +0100

    Name fix

--- api/fetchProducts.ts
+++ api/fetchProducts.ts
@@ -1,6 +1,6 @@
 import { ProductsModel } from "../models";
 
-export const useGetProducts = async (): Promise<ProductsModel> => {
+export const fetchProducts = async (): Promise<ProductsModel> => {
   const response = await fetch("../dummy-data.json");
   return response.json();
 };
